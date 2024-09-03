04c78b25f6971f796ce043cbe9fb16ddb2589a1b
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2024-09-03 18:02:06 -0300

    Add Python parser dependency
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rustpython_parser::{ast, Parse};
 fn main() {
-    println!("Hello, world!");
+    let python_source = "print('Hello world')";
+    let python_statements = ast::Suite::parse(python_source, "").unwrap();
+    let python_expr = ast::Expr::parse(python_source, "").unwrap();
+    println!("{:?}", python_statements);
+    println!("{:?}", python_expr);
 }
