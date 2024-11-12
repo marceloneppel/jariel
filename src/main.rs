e620da88dd1931bed6fbc9f0e751b96c2f665ca2
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2024-11-12 15:39:40 -0300

    Parse charm code
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use rustpython_parser::{ast, Parse};
+use std::fs;
 fn main() {
-    let python_source = "print('Hello world')";
-    let python_statements = ast::Suite::parse(python_source, "").unwrap();
-    let python_expr = ast::Expr::parse(python_source, "").unwrap();
+    // let python_source = "print('Hello world')";
+    let python_source =
+        fs::read_to_string("charm/src/charm.py").expect("Should have been able to read the file");
+    // println!("{}", python_source);
+    let python_statements = ast::Suite::parse(&python_source, "").unwrap();
+    // let python_expr = ast::Expr::parse(&python_source, "").unwrap();
     println!("{:?}", python_statements);
-    println!("{:?}", python_expr);
+    // println!("{:?}", python_expr);
 }
